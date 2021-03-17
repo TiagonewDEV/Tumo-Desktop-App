68a3ca9ed7136ca331e1bed5c6ed2767fc8f569f
Louis Gallet <louis.gallet@tumo.world>
2021-03-17 18:36:17 +0100

    first release + app 1.0

---
+++ index.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow, screen } = require('electron')
+
+let win
+app.whenReady().then(() => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  win = new BrowserWindow({ width, height })
+  win.loadURL('https://fr.tumo.world')
+})
